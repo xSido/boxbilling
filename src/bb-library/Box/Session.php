1eaf1b893de289af2e6bce8e7b5da67b2dc19f65
Yağızhan <yagizhan49@protonmail.com>
2021-08-25 20:54:46 +0300

    Sync

--- src/bb-library/Box/Session.php
+++ src/bb-library/Box/Session.php
@@ -13,31 +13,6 @@
 
 class Box_Session
 {
-    public function __construct($handler)
-    {
-        session_set_save_handler(
-            array($handler, 'open'),
-            array($handler, 'close'),
-            array($handler, 'read'),
-            array($handler, 'write'),
-            array($handler, 'destroy'),
-            array($handler, 'gc')
-        );
-        if(php_sapi_name() !== 'cli'){
-            $currentCookieParams = session_get_cookie_params();
-            $currentCookieParams["httponly"] = true;
-
-            session_set_cookie_params(
-                $currentCookieParams["lifetime"],
-                $currentCookieParams["path"],
-                $currentCookieParams["domain"],
-                $currentCookieParams["secure"],
-                $currentCookieParams["httponly"]
-            );
-            session_start();
-        }
-    }
-
     public function getId()
     {
         return session_id();
